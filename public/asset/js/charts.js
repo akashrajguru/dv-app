4093c120152ffac888b91a4236e7ce8d8d6963e8
Akash Rajguru <akashrajguru@icloud.com>
2017-03-25 16:49:59 +0000

    removed unwanted code

--- public/asset/js/charts.js
+++ public/asset/js/charts.js
@@ -1,67 +1,19 @@
-$.get('https://www.quandl.com/api/v3/datasets/ECB/EURINR', function (result){
+// Wait until the body has loaded
+$(document).ready(function(){
+    console.log('js');
+    $.get('https://www.quandl.com/api/v3/datasets/ECB/EURINR', function (result){
     //console.log("result is ", result.dataset.data);
 
     var data = result.dataset.data;
     //console.log("data is ",data);
     processdata(data);
 
- /*    Highcharts.chart('chart', {
-        chart: {
-            zoomType: 'x'
-        },
-        title: {
-            text: 'EUR to INR exchange rate over time'
-        },
-        subtitle: {
-            text: document.ontouchstart === undefined ?
-                    'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
-        },
-        xAxis: {
-            type: 'datetime'
-        },
-        yAxis: {
-            title: {
-                text: 'Exchange rate'
-            }
-        },
-        legend: {
-            enabled: false
-        },
-        plotOptions: {
-            area: {
-                fillColor: {
-                    linearGradient: {
-                        x1: 0,
-                        y1: 0,
-                        x2: 0,
-                        y2: 1
-                    },
-                    stops: [
-                        [0, Highcharts.getOptions().colors[0]],
-                        [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-                    ]
-                },
-                marker: {
-                    radius: 2
-                },
-                lineWidth: 1,
-                states: {
-                    hover: {
-                        lineWidth: 1
-                    }
-                },
-                threshold: null
-            }
-        },
-
-        series: [{
-            type: 'area',
-            name: 'USD to EUR',
-            data: data
-        }]
-    });*/
     convertDate("2017-03-23");
+    });
 });
+
+
+
 
 function processdata(array){
     //console.log('array in processdata', array);
@@ -140,62 +92,3 @@ function drawChart(data)
     });
 
 }
-
-$.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=usdeur.json&callback=?', function (data) {
-    //console.log('data',data);
-    /*Highcharts.chart('chart', {
-        chart: {
-            zoomType: 'x'
-        },
-        title: {
-            text: 'USD to EUR exchange rate over time'
-        },
-        subtitle: {
-            text: document.ontouchstart === undefined ?
-                    'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
-        },
-        xAxis: {
-            type: 'datetime'
-        },
-        yAxis: {
-            title: {
-                text: 'Exchange rate'
-            }
-        },
-        legend: {
-            enabled: false
-        },
-        plotOptions: {
-            area: {
-                fillColor: {
-                    linearGradient: {
-                        x1: 0,
-                        y1: 0,
-                        x2: 0,
-                        y2: 1
-                    },
-                    stops: [
-                        [0, Highcharts.getOptions().colors[0]],
-                        [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-                    ]
-                },
-                marker: {
-                    radius: 2
-                },
-                lineWidth: 1,
-                states: {
-                    hover: {
-                        lineWidth: 1
-                    }
-                },
-                threshold: null
-            }
-        },
-
-        series: [{
-            type: 'area',
-            name: 'USD to EUR',
-            data: data
-        }]
-    });*/
-});
